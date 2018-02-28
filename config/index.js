0bd2ac44165e1b1e2a4e0fa1254de82afb9b1886
BrownHu <1104524352@qq.com>
2018-02-28 17:56:51 +0800

    预览

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/hole-in-the-wall/dist/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/hole-in-the-wall/dist/',
 
     /**
      * Source Maps
